579ccc907af9ac925f1a1713ab86a863e968aa69
Cuvelier Francois <cuvelier@math.univ-paris13.fr>
2016-12-10 15:55:28 +0100

    Debut creation archives avec Copyright auto (suite 4)

--- +fcTools/version.m
+++ +fcTools/version.m
@@ -1,7 +1,7 @@
-function version=version()
-% FUNCTION fcTools.version: Returns the version of this toolbaox/package 
+function v=version()
+% FUNCTION fcTools.version: Returns the version of this toolbox/package 
 %   as a string.
 %
 % <COPYRIGHT>
-  version='0.0.2';
+  v='0.0.2';
 end
