3921a164670e206234d06cae56b0781856a80f25
Cuvelier Francois <cuvelier@math.univ-paris13.fr>
2016-12-11 06:34:26 +0100

    Modif init

--- +fcTools/init.m
+++ +fcTools/init.m
@@ -7,12 +7,16 @@ function varargout=init(varargin)
   p.addParamValue('verbose',0,@isscalar);
   p.parse(varargin{:});
   R=p.Results;
+  fullname=mfilename('fullpath');
+  I=strfind(fullname,filesep);
+  mypath=fullname(1:(I(end-1)-1));
   if p.Results.verbose>0
     fprintf('Using mfcTools toolbox [%s]\n',fcTools.version());
+    if p.Results.verbose>1, fprintf('   locate in %s\n',mypath);end
   end
   if nargout==1
     fullname=mfilename('fullpath');
     I=strfind(fullname,filesep);
-    varargout{1}=fullname(1:(I(end-1)-1));
+    varargout{1}=mypath;
   end
 end
