1647c3395d5a091330a8f1cfd62eea2927dbc61a
Cuvelier Francois <cuvelier@math.univ-paris13.fr>
2016-12-16 16:20:48 +0100

    Mise a jour! pour que dalle

--- +fc_tools/+graphics/ColumnLegend.m
+++ +fc_tools/+graphics/ColumnLegend.m
@@ -1,4 +1,4 @@
 function [legend_h,object_h,plot_h,text_strings] = ColumnLegend(numcolumns,handles , str, varargin)
 % fc_tools.graphics.ColumnLegend : just an interface to fc_tools.graphics.columnlegend.columnlegend
-[legend_h,object_h,plot_h,text_strings] =fc_tools.graphics.columnlegend.columnlegend(numcolumns,handles , str, varargin);
+  [legend_h,object_h,plot_h,text_strings] =fc_tools.graphics.columnlegend.columnlegend(numcolumns,handles , str, varargin);
 end
