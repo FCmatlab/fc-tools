f7eae9142c3b49bf9e57e2da8f7499242cc37965
Cuvelier Francois <cuvelier@math.univ-paris13.fr>
2017-12-28 07:14:59 +0100

    MaJ

--- +fc_tools/+sys/getOSinfo.m
+++ +fc_tools/+sys/getOSinfo.m
@@ -14,13 +14,31 @@ function OS = getOSinfo()
 end
 
 function OS=getOSinfo_Unix()
-  [status,result]=fc_tools.sys.sec_system('lsb_release -a');
-  Lines = textscan( result, '%s', 'Delimiter', '\n' );
-  Lines=Lines{1};
-  OS.distributor=fc_tools.sys.find_keyvalue('Distributor ID',':',Lines);
-  OS.description=fc_tools.sys.find_keyvalue('Description',':',Lines);
-  OS.release=fc_tools.sys.find_keyvalue('Release',':',Lines);
-  OS.codename=fc_tools.sys.find_keyvalue('Codename',':',Lines);
+  [status,result]=system('lsb_release -a');
+  if status==0
+    Lines = textscan( result, '%s', 'Delimiter', '\n' );
+    Lines=Lines{1};
+    OS.distributor=fc_tools.sys.find_keyvalue('Distributor ID',':',Lines);
+    OS.description=fc_tools.sys.find_keyvalue('Description',':',Lines);
+    OS.release=fc_tools.sys.find_keyvalue('Release',':',Lines);
+    OS.codename=fc_tools.sys.find_keyvalue('Codename',':',Lines);
+  elseif exists('/etc/os-release','file')
+      fid=fopen('/etc/os-release','r');
+      Lines = textscan( fid, '%s', 'Delimiter', '\n' );
+      fclose(fid)
+      Lines=Lines{1};
+      OS.distributor=fc_tools.sys.find_keyvalue('ID','=',Lines);
+      OS.description=fc_tools.sys.find_keyvalue('PRETTY_NAME','=',Lines);
+      OS.release=fc_tools.sys.find_keyvalue('VERSION_ID','=',Lines);
+      %OS.codename=fc_tools.sys.find_keyvalue('Codename',':',Lines);
+      OS.codename='Unknow';
+  else
+      %fprintf('[fc-tools] Unable to guess Unix system name\n');
+      OS.distributor='UNIX';
+      OS.description='Unknow';
+      OS.release='Unknow';
+      OS.codename='Unknow';
+  end
   [status,result]=fc_tools.sys.sec_system('uname -m');
   OS.arch=strtrim(result);
   OS.shortname=OS.distributor;
