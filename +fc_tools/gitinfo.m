6fb27036ab4227681cdee71e99af9b3bfa6ee8a1
Cuvelier F <cuvelier@math.univ-paris13.fr>
2018-04-25 13:33:53 +0200

    MaJ

--- +fc_tools/gitinfo.m
+++ +fc_tools/gitinfo.m
@@ -3,9 +3,19 @@ function S=gitinfo()
 %
 %    <COPYRIGHT>
 %
-  tag='';
-  commit='';
-  strdate='';
-  strtime='';
-  S=struct('tag',tag,'commit',commit,'date',strdate,'time',strtime);
+  rep=fc_tools.path();
+  if fc_tools.git.isrepository(rep)
+    S=struct('name',  fc_tools.git.get_name(rep), ...
+             'tag',   fc_tools.git.get_tag(rep), ...
+             'commit',fc_tools.git.get_commit(rep), ...
+             'date',  fc_tools.git.get_date(rep), ...
+             'time',  fc_tools.git.get_time(rep), ...
+             'status',fc_tools.git.isup2date(rep));
+  else
+    tag='';
+    commit='';
+    strdate='';
+    strtime='';
+    S=struct('name','fc-tools','tag',tag,'commit',commit,'date',strdate,'time',strtime,'status',-1);
+  end
 end
