ea3d8d2104c720330ce262923d2b942197f8e00d
Cuvelier F <cuvelier@math.univ-paris13.fr>
2018-04-26 08:27:32 +0200

    MaJ

--- +fc_tools/+git/isrepository.m
+++ +fc_tools/+git/isrepository.m
@@ -1,5 +1,6 @@
 function bool=isrepository(rep)
 % check if rep directory is a git repository
   if nargin==0,rep='';end;
-  bool=~isempty(fc_tools.git.get_name(rep));
+  bool=(exist([rep,filesep,'.git'],'dir')==7);
+  % bool=~isempty(fc_tools.git.get_name(rep)); 
 end
