b14eb15e95e0e8086a670b56acafeb77a2a1cc0b
F. Cuvelier <cuvelier@math.univ-paris13.fr>
2019-03-06 07:03:55 +0100

    MaJ

--- +fc_tools/info.m
+++ +fc_tools/info.m
@@ -6,22 +6,23 @@ function info(verbosity)
   if nargin==0, verbosity=1;end
   [pkg,pkgs]=fc_tools.packages();
   assert(ismember(verbosity,[1,2]) , ['[fc_',pkg,'.info] Input parameter must be 1 or 2'])
-  if verbosity>1
-  fprintf('Using %s\n',fc_tools.utils.fcpackagestr(['fc_',pkg],verbosity))
+  if verbosity==1,info1(pkg,pkgs); else, info2(pkg,pkgs);end
+end
+
+function info1(pkg,pkgs)
   n_pkgs=length(pkgs);
-  if n_pkgs>1
-    for i=1:n_pkgs-1
-      fprintf('  %s\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{i}],verbosity))
-    end
-    if n_pkgs>1, fprintf('  and ');end
-    fprintf('  and %s\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{end}],verbosity))
-    else
-      fprintf('Using %s with ',fc_tools.utils.fcpackagestr(['fc_',pkg],verbosity))
-      for i=1:n_pkgs-1
-        fprintf('%s, ',fc_tools.utils.fcpackagestr(['fc_',pkgs{i}],verbosity))
-      end
-      if n_pkgs>1, fprintf('  and ');end
-      fprintf('%s.\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{end}],verbosity))
-    end
+  fprintf('Using %s with ',fc_tools.utils.fcpackagestr(['fc_',pkg],1))
+  for i=1:n_pkgs-1
+    fprintf('%s, ',fc_tools.utils.fcpackagestr(['fc_',pkgs{i}],1))  
   end
+  fprintf('%s.\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{end}],1))  
+end
+
+function info2(pkg,pkgs)
+  n_pkgs=length(pkgs);
+  fprintf('Using %s with:\n',fc_tools.utils.fcpackagestr(['fc_',pkg],2))
+  for i=1:n_pkgs-1
+    fprintf('    %s,\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{i}],2))  
+  end
+  fprintf('   %s.\n',fc_tools.utils.fcpackagestr(['fc_',pkgs{end}],2))  
 end
